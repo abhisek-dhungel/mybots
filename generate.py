554e0dd12893134326bbce80025577ac1e78a011
abhisek-dhungel <abhisek.dhungel001@gmail.com>
2025-10-16 19:34:55 +0545

    create world

--- generate.py
+++ generate.py
@@ -18,24 +18,7 @@ def Create_World():
     pyrosim.End()
 
 
-def Create_Robot():
-    pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[
-        length, width, height])
 
-    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso",
-                       child="BackLeg", type="revolute", position=[0.5, 0, 1])
-
-    pyrosim.Send_Cube(
-        name="BackLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
-
-    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso",
-                       child="FrontLeg", type="revolute", position=[-0.5, 0, 1])
-
-    pyrosim.Send_Cube(
-        name="FrontLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
-
-    pyrosim.End()
 
 
 Create_World()
