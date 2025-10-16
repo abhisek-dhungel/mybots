91c9b2a29e84e2ef92ce0a48884f3a7b8e083976
abhisek-dhungel <abhisek.dhungel001@gmail.com>
2025-10-16 09:24:09 +0545

    Created generate.py for one link

--- generate.py
+++ generate.py
@@ -2,10 +2,8 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")  # Name of the world file
 
-pyrosim.Send_Cube(
-    name="Box", 
-    pos=[0,0,0.5],  # x, y, z position
-    size=[1,1,1]    # length, width, height
-)
+pyrosim.Send_Cube(name="box", pos=[0,0,0.5] , size=[1,1,1])
 
 pyrosim.End()
+
+
