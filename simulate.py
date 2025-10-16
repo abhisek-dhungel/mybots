d4f409158b734fb519072056fc3adb08ad491b43
abhisek-dhungel <abhisek.dhungel001@gmail.com>
2025-10-16 11:10:46 +0545

    Simulating one link from box.sdf

--- simulate.py
+++ simulate.py
@@ -2,9 +2,9 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
-# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-# Load the box
+# Load the world created by Pyrosim
 p.loadSDF("box.sdf")
 
 for i in range(1000):
