bd2a67e11bc28d3bd2790da6c2cfc0e2dbdb44b0
abhisek-dhungel <abhisek.dhungel001@gmail.com>
2025-10-16 17:49:19 +0545

    joints completed

--- generate.py
+++ generate.py
@@ -1,19 +1,46 @@
-from pyrosim.pyrosim import Pyrosim
+import pyrosim.pyrosim as pyrosim
 
-num_blocks = 10
-length = width = height = 1
+length = 1
+width = 1
+height = 1
 x = 0
-y = 0
+y = 0.5
+z = 0
+n = 0
 
-pyrosim = Pyrosim("tower.sdf")
-pyrosim.Start_SDF()
+# [z,x,y]
 
-for i in range(num_blocks):
-    z = (height/2) + i * height
-    pyrosim.Send_Cube(name=f"Block{i}", pos=[x, y, z], size=[length, width, height])
-    # shrink each block for visual effect
-    length *= 0.9
-    width  *= 0.9
-    height *= 0.9
 
-pyrosim.End()
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(
+        name="Box", pos=[-10, -5, 1], size=[length, width, height])
+    pyrosim.End()
+
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="BackLeg", pos=[0, 0.5, 0.5], size=[
+                      length, width, height])
+
+    pyrosim.Send_Joint(name="BackLeg_Torso", parent="BackLeg",
+                       child="Torso", type="revolute", position=[0, 1, 1])
+
+    pyrosim.Send_Cube(
+        name="Torso", pos=[0, 0.5, 0.5], size=[length, width, height])
+
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso",
+                       child="FrontLeg", type="revolute", position=[0, 1.0, 0])
+
+    pyrosim.Send_Cube(
+        name="FrontLeg", pos=[0, 0.5, -0.5], size=[length, width, height])
+
+    pyrosim.End()
+
+
+Create_World()
+Create_Robot()
+
+
+# green line x
+# red line z
--- simulate.py
+++ simulate.py
@@ -3,15 +3,15 @@ import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8, physicsClient)
 
+p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("tower.sdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
 
-for i in range(1000):
+for x in range(1000):
     p.stepSimulation()
-    time.sleep(1/60)
-
+    time.sleep(1/30)
+    print(x)
 p.disconnect()
